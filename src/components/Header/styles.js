cac1535099472f8d6d48e015d3538bb8295f6242
dimaspaiva <dimasalpaiva@gmail.com>
2020-02-28 09:29:33 -0300

    header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 80px;
+  padding: 0 30px;
+  background: #454545;
+  color: #eaeaea;
+
+  display: flex;
+  align-items: center;
+`;
