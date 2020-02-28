90b8893fb46fa2074bf94e6b39c531acd9812718
dimaspaiva <dimasalpaiva@gmail.com>
2020-02-28 11:04:47 -0300

    styles first step

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,6 +1,27 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: relative;
+  background: #909090;
+  border-radius: 3px;
+  margin-bottom: 15px;
+  padding: 12px;
+
+  box-shadow: 0 1px 6px 0 rgba(30, 30, 30, 0.3);
+  border-top: 18px solid rgba(180, 180, 180, 0.3);
+  cursor: grab;
+
+  header {
+    position: absolute;
+    top: -8px;
+    left: 15px;
+  }
+
+  p {
+    font-weight: 500;
+    line-height: 20px;
+  }
+
   img {
     width: 24px;
     height: 24px;
@@ -9,4 +30,10 @@ export const Container = styled.div`
   }
 `;
 
-export const Label = styled.span``;
+export const Label = styled.span`
+  width: 12px;
+  height: 12px;
+  border-radius: 1px;
+  display: inline-block;
+  background: ${props => props.color};
+`;
--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -1,7 +1,37 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  margin-top: 21px;
   padding: 0 15px;
   height: calc(100% - 60px);
   flex: 0 0 320px;
+
+  & + div {
+    border-left: 1px solid rgba(30, 30, 30, 0.5);
+  }
+
+  header {
+    display: felx;
+    justify-content: space-between;
+    align-items: center;
+
+    h2 {
+      font-weight: 500;
+      font-size: 18px;
+      padding: 0 9px;
+    }
+
+    button {
+      width: 42px;
+      height: 42px;
+      border-radius: 100%;
+      background: #606060;
+      border: 0;
+      cursor: pointer;
+    }
+  }
+
+  ul {
+    margin-top: 15px;
+  }
 `;
